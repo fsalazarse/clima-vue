0b7aadca4ea88e29fcb0d76dbc497b69536cebe5
fabian <fsalazar@hyl.cl>
2023-07-24 17:55:59 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+   import Formulario from './components/Formulario.vue';
+</script>
+
+<template>
+  <h1 class="titulo">Buscar Clima</h1>
+
+  <div class="contenedor buscar-clima">
+
+    <Formulario />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,38 @@
+<script setup>
+    import {reactive} from 'vue'
+
+    const busqueda = reactive({
+        ciudad: '',
+        pais: ''
+    })
+
+const paises = [
+    { codigo: 'US', nombre: 'Estados Unidos' },
+    { codigo: 'MX', nombre: 'México' },
+    { codigo: 'AR', nombre: 'Argentina' },
+    { codigo: 'CO', nombre: 'Colombia' },
+    { codigo: 'CR', nombre: 'Costa Rica' },
+    { codigo: 'ES', nombre: 'España' },
+    { codigo: 'PE', nombre: 'Perú' }
+]
+</script>
+
+<template>
+    <form class="formulario">
+        <div class="campo">
+            <label for="ciudad">Ciudad</label>
+            <input type="text" id="ciudad" placeholder="ciudad" v-model="busqueda.ciudad">
+        </div>
+        <div class="campo">
+            <label for="ciudad">Pais</label>
+            <select id="pais" v-model="busqueda.pais">
+                <option value="">-- Seleccione un pais --</option>
+                <option v-for="pais in paises" :value="pais.codigo">{{pais.nombre}}-</option>
+            </select>
+        </div>
+
+        <input type="submit" value="Consultar clima">
+    </form>
+</template>
+
+<style></style>
